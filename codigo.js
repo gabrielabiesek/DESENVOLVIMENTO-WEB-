7600a367c16f0ef19445e7ee17fdd517f365882a
gabrielabiesek <gabrielabiesek24@gmail.com>
2025-02-25 21:44:03 -0300

    Add files via upload
    AULA DIA 25/02

--- codigo.js
+++ codigo.js
@@ -1,19 +1,26 @@
-/*console.log("oi");
 
-var x = 4;
+
+/*var x = prompt("Digite um valor");
 
     if (x % 2 == 0){
-        console.log("É par. ");
+        document.write("É par. ");
     }
-    else;
-        console.log("É ímpar. ");*/
-
-/*if (x % x, x % 1){
-    console.log("É número primo.");
+    else{
+        document.write("É ímpar. ");
+    }
+    
+    var divisores = 0;
+for (var i = 0; i <= x; i++){
+ if(x % i == 0){
+    divisores++;
+ } 
 }
-else;
-    console.log("Não é número primo."); */
-
+    if(divisores == 2){
+    document.write("É número primo.");
+}
+    else{
+    document.write("Não é número primo.")
+}*/
 /*var y; // Divisor
 var contador = 0; //Conta quantos divisores tem
 
@@ -30,39 +37,44 @@ if (x % y == 0){
 else;
     console.log("O número não é primo. ")*/
 
-/*var opcao = 3;
-var num1 = 4;
-var num2 = 2;
+
+   // console.log("A opção é inválida!")
+
+var opcao = prompt("Digite uma opção");
+var num1 = parseInt(prompt("Digite um valor"));
+var num2 = parseInt(prompt("Digite outro valor"));
 var resultado;
+var x=1;
 
 if (opcao == 0){   //Essa opção é soma
 resultado = num1 + num2;
-    console.log(resultado);
+    document.write(resultado);
 }
 
 if (opcao == 1){   //Essa opção é subtração
 resultado = num1 - num2;
-    console.log(resultado);
+    document.write(resultado);
 }
 
 if (opcao == 2){   //Essa opção é multiplicação
 resultado = num1 * num2;
-    console.log(resultado);
+    document.write(resultado);
 }
 
 if (opcao == 3){   //Essa opção é divisão
-resultado = num1 % num2;
-    console.log(resultado);
+resultado = num1 / num2;
+    document.write(resultado);
 }
 
-if (opcao == 4){
+if (opcao == 4){   //Essa opção é a potenciação
+    resultado = num1;
+    for (x < num2){
+        resultado = resultado * num1;
+        x++
+    }
 
 }
-
-else;
-   // console.log("A opção é inválida!")*/
-
-var num_sequencia = 13;
+/*var num_sequencia = 13;
 
 var num1 = 0;
 var num2 = 1;
@@ -71,11 +83,20 @@ var resultado;
 resultado = num1 + num2;
 var i = 0;
 
-while (i < num_sequencia){
+while (i > num_sequencia){
     num1 = num2;
     num2 = resultado;
     resultado = num1 + num2;
     i++;
 }
    
-document.write(resultado);
+document.write(resultado);*/
+
+/*Coloque um conjunto de números inteiros em ordem crescente. 
+ 
+ 	Entrada: vetor de números e tamanho da lista. 
+ 	Saída: vetor ordenado de maneira crescente. */
+
+//var lista=[4,8,1,9,6]
+
+
